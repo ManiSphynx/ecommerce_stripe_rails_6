776dfd0886fb5ae447484356ed43d83052db2fee
Emanuel Romero Madrigal <emanuel.romero@hotmail.es>
2023-02-26 16:46:51 -0600

    feat: adding association of an article with an order to a user

---
+++ app/models/order.rb
@@ -0,0 +1,17 @@
+class Order < ApplicationRecord
+  # Associations
+  belongs_to :user
+  has_many :order_items
+
+  # Callbacks
+  after_save :add_articles_to_user, if: :paid_order?
+
+  def paid_order?
+    status == 'paid'
+  end
+
+  def add_articles_to_user
+    user.article_ids += order_items.pluck(:article_id)
+    user.save
+  end
+end
