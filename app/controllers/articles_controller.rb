1157a2341627688c7601b7f0bef6b62f0819c8f4
Emanuel Romero Madrigal <emanuel.romero@hotmail.es>
2023-02-26 16:43:26 -0600

    feat: adding create method definition and creating a new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,15 +9,29 @@ class ArticlesController < ApplicationController
     @article = policy_scope(Article).find(params[:id])
   end
 
-  def edit
-  end
+  def edit; end
 
-  def update
-  end
+  def update; end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      flash[:success] = 'Article successfully created'
+      redirect_to @article
+    else
+      flash[:error] = 'Something went wrong'
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body, :price, :private)
   end
 end
