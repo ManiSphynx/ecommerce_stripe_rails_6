4be9ff769b7edc3f85f7ae540a27230b4de67972
Emanuel Romero Madrigal <emanuel.romero@hotmail.es>
2023-02-26 16:47:18 -0600

    feat: adding validation to show a complete article or not to show it

--- app/policies/article_policy.rb
+++ app/policies/article_policy.rb
@@ -4,6 +4,9 @@ class ArticlePolicy < ApplicationPolicy
   end
 
   def show?
-    user || !record.private
+    return true unless record.private?
+    return false unless user
+
+    user.article_ids.include?(record.id)
   end
 end
