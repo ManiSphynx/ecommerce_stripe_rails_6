464a4f7a3bcf5b91d880bda8142ed507bf6c3ddf
Emanuel Romero Madrigal <emanuel.romero@hotmail.es>
2023-02-26 16:45:08 -0600

    feat: adding create method definition and adding items to an order if it doesn't exist

---
+++ app/controllers/order_items_controller.rb
@@ -0,0 +1,24 @@
+class OrderItemsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    order_item = current_order.order_items.new(article_id: order_item_params)
+
+    if order_item.save
+      redirect_to root_path, notice: 'Article added to cart'
+    else
+      redirect_to articles_path(id: order_item_params), alert: 'Article was not added to cart'
+    end
+  end
+
+  private
+
+  def current_order
+    order = Order.where(user_id: current_user.id, status: 'created').order(updated_at: :desc).first
+    order || Order.create(user_id: current_user.id)
+  end
+
+  def order_item_params
+    params.require(:article_id)
+  end
+end
