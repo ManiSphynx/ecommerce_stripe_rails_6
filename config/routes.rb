915a1d3824e3d98a8f2da14d07b462fbf238d5f4
Emanuel Romero Madrigal <emanuel.romero@hotmail.es>
2023-02-26 19:24:45 -0600

    feat: add checkout route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   resources :articles, except: [:destroy]
   resources :order_items, only: [:create]
+  resources :orders, only: [:show] do
+    get '/current', to: 'orders#current', on: :collection
+  end
+  resources :checkout, only: [:create]
 end
