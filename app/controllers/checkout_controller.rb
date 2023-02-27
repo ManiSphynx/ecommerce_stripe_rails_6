f275b645875ebea3c5ee5c65a8ef4c4e9a6d3452
Emanuel Romero Madrigal <emanuel.romero@hotmail.es>
2023-02-26 19:23:12 -0600

    feat: add checkout controller and redirect to stripe

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,18 @@
+class CheckoutController < ApplicationController
+  def create
+    order = Order.find(params[:order_id])
+
+    prices = order.articles.map do |article|
+      { price: article.stripe_pricing_id, quantity: 1 }
+    end
+
+    session = Stripe::Checkout::Session.create({
+                                                 cancel_url: order_url(order),
+                                                 success_url: root_url,
+                                                 mode: 'payment',
+                                                 payment_method_types: ['card'],
+                                                 line_items: prices
+                                               })
+    redirect_to session.url
+  end
+end
