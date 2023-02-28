ff8a748982cf208ad9540d1addfdc07dc1aacf8d
Emanuel Romero Madrigal <emanuel.romero@hotmail.es>
2023-02-27 18:09:24 -0600

    chore: add migration for stripe_payment id field in orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_26_222325) do
+ActiveRecord::Schema.define(version: 2023_02_27_233531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2023_02_26_222325) do
     t.string "status", default: "created"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "stripe_payment_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
